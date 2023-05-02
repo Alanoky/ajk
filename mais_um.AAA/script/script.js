a329ee3f52c0133d7cce7d3b95788449cbc79dc6
Alanoky <125393521+Alanoky@users.noreply.github.com>
2023-05-02 15:47:39 -0300

    Add files via upload

---
+++ mais_um.AAA/script/script.js
@@ -0,0 +1,55 @@
+// cria um botão de "fechar" e o adiciona em cada item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// cliar em um botão de fechar esconderá o item da lista
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// adiciona um simbolo de "concluido" ao clicar em um item da lista
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+//cria um novo item na lista ao clicar no botão de "adicionar"
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("input").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("escreva algo.");
+    } else {
+      document.getElementById("ul").appendChild(li);
+    }
+    document.getElementById("input").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
